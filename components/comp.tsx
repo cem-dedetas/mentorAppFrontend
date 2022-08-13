d03150084646222362baeef98422f86bfa17d035
dorukErgun <doruk.ergun2017@gtu.edu.tr>
2022-08-13 09:59:09 +0300

    I really cant

--- components/comp.tsx
+++
@@ -1,9 +0,0 @@
-import { useTranslation } from "next-i18next";
-
-const Comp = () => {
-    const { t } = useTranslation();
-
-    return (<h1>{t('loginsss')}</h1>);
-}
-
-export default Comp;
--- next.config.js
+++ next.config.js
@@ -3,7 +3,15 @@ const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
   reactStrictMode: true,
-  i18n
+  i18n,
+  publicRuntimeConfig: {
+    apiUrl: process.env.NODE_ENV === 'development'
+      ? 'http://localhost:5127/api/' // development api
+      : '/api/v1/', // production api
+    authUrl: process.env.NODE_ENV === 'development'
+      ? 'http://localhost/SurfaceAuth/api/login' // development auth api
+      : process.env.AUTH_URL, // production auth api
+  }
 }
 
 module.exports = nextConfig
